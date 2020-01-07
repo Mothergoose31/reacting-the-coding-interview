5f0de553af3be95e0099cf951d32812f0870461e
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-01-07 11:01:13 -0800

    contributors

--- src/codeExample.jsx
+++ src/codeExample.jsx
@@ -3,7 +3,14 @@ import { render } from "react-dom";
 import Highlight, { defaultProps } from "prism-react-renderer";
 
 export default class codeExample extends Component {
+const exampleCode = 
+(function someDemo() {
+  var test = "Hello World!";
+  console.log(test);
+})();
 
+return () => <App />;
+;
 
     render() {
         return (
