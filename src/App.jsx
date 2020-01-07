01a0f7a7cc18d1336c7199aa2f40fc9a8b483078
unknown <vtdanh0701@gmail.com>
2020-01-07 15:25:57 -0800

    created splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import NavBar from './components/navbar/NavBar'
 import './App.css';
+import SplashScreen from './components/splashScreen/SplashScreen'
 
 function App() {
+  const [isLoading,setLoading] = useState(true) // set initial loading state to true 
+  
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false)
+    },2000) //set splashing screen time on mount to 2s, can disable for development
+  },[])
+  
   return (
   <>
-  <NavBar/>
-    <div>
-      <h1>Hello</h1>
-    </div>
+  {isLoading? <SplashScreen/> : 
+              // if loading, display splash screen, else display app
+              <div>
+                <NavBar/>
+                <div>
+                  <h1>Hello</h1>
+                </div>
+              </div>         
+  }
   </>
   )
 }
