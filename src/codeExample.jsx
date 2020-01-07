565fc03fce4c84b03ba714d7defdbe92af7731db
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-01-07 10:46:49 -0800

    code example component

---
+++ src/codeExample.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { render } from "react-dom";
+import Highlight, { defaultProps } from "prism-react-renderer";
+
+export default class codeExample extends Component {
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
