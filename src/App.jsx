859d5e3ccfb0b0975c72112b303ba870061fb47c
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-01-07 13:22:19 -0800

    nav Bar started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavBar from './components/navbar/NavBar'
 import './App.css';
 
 function App() {
   return (
   <>
+  <NavBar/>
     <div>
       <h1>Hello</h1>
     </div>
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <ul id="nav">
+                    <li><a href="#">Burger Menu </a></li>
+                    <li><a href="#">Dank Logo</a></li>
+                    <li><a href="#"> About</a></li>
+                </ul>
+            </div>
+        )
+    }
+}
