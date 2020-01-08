5883bb8b9f31ef5fb37156ea92bae6353ed584d9
unknown <vtdanh0701@gmail.com>
2020-01-07 16:27:49 -0800

    passing state example

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,28 @@ import SplashScreen from './components/splashScreen/SplashScreen'
 
 function App() {
   const [isLoading,setLoading] = useState(true) // set initial loading state to true 
+  const [render,setRender] = useState('none')
   
-  useEffect(()=>{
-    setTimeout(()=>{
-      setLoading(false)
-    },2000) //set splashing screen time on mount to 2s, can disable for development
-  },[])
+  // useEffect(()=>{
+  //   setTimeout(()=>{
+  //     setLoading(false)
+  //   },2000) //set splashing screen time on mount to 2s, can disable for development
+  // },[]);
+  
+  const handleChange = data => {
+    setRender(data)
+  }
   
   return (
-  <>
-  {isLoading? <SplashScreen/> : 
-              // if loading, display splash screen, else display app
+          <>      
               <div>
-                <NavBar/>
+                <NavBar handleChange={handleChange}/>
                 <div>
                   <h1>Hello</h1>
                 </div>
+                {render}
               </div>         
-  }
-  </>
+          </>
   )
 }
 
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import SplashScreen from '../splashScreen/SplashScreen';
+import IsUnique from '../problems/isUnique/IsUnique'
 
 export default class NavBar extends Component {
+    constructor(props){
+        super(props)
+    }
     render() {
         return (
             <div>
                 <ul id="nav">
+                    <button onClick={()=>this.props.handleChange(<SplashScreen/>)}>SplashScreen</button>
+                    <button onClick={()=>this.props.handleChange(<IsUnique/>)}>IsUnique</button>
                     <li><a href="#">Burger Menu </a></li>
                     <li><a href="#">Dank Logo</a></li>
                     <li><a href="#"> About</a></li>
