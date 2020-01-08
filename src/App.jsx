5df84db6673b5c9ece0d44d9b657511bc8a3d5bd
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-01-08 14:55:49 -0800

    tester sidebar for now

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, {useState,useEffect} from 'react';
 import NavBar from './components/navbar/NavBar'
 import './App.css';
 import SplashScreen from './components/splashScreen/SplashScreen'
+import SideMenu from "./components/sideMenu/SideMenu";
 
 function App() {
   const [isLoading,setLoading] = useState(true) // set initial loading state to true 
@@ -13,17 +14,20 @@ function App() {
   },[])
   
   return (
-  <>
+  <div id="App">
   {isLoading? <SplashScreen/> : 
-              // if loading, display splash screen, else display app
-              <div>
-                <NavBar/>
-                <div>
-                  <h1>Hello</h1>
-                </div>
-              </div>         
+    <>           
+    {/* // if loading, display splash screen, else display app */}
+      <SideMenu pageWrapId={"page-wrap"} outerContainerId={"App"} />
+      <div id ="page-wrap">
+        <NavBar/>
+        <div>
+          <h1>Hello</h1>
+        </div>
+      </div>  
+    </>         
   }
-  </>
+  </div>
   )
 }
 
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -5,9 +5,10 @@ export default class NavBar extends Component {
         return (
             <div>
                 <ul id="nav">
-                    <li><a href="#">Burger Menu </a></li>
+                    
                     <li><a href="#">Dank Logo</a></li>
                     <li><a href="#"> About</a></li>
+                    
                 </ul>
             </div>
         )
