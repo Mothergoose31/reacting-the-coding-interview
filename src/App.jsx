4d73dd93ffc84345fd14eb2e795b33f0ace81985
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-01-09 09:21:32 -0800

    removed crap side bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React, {useState,useEffect} from 'react';
 import NavBar from './components/navbar/NavBar'
 import './App.css';
 import SplashScreen from './components/splashScreen/SplashScreen'
-import SideMenu from "./components/sideMenu/SideMenu";
+
 
 function App() {
   const [isLoading,setLoading] = useState(true) // set initial loading state to true 
   const [render,setRender] = useState('none')
   
-  // useEffect(()=>{
-  //   setTimeout(()=>{
-  //     setLoading(false)
-  //   },2000) //set splashing screen time on mount to 2s, can disable for development
-  // },[]);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false)
+    },2000) //set splashing screen time on mount to 2s, can disable for development
+  },[]);
   
   const handleChange = data => {
     setRender(data)
@@ -23,7 +23,7 @@ function App() {
   {isLoading? <SplashScreen/> : 
     <>           
     {/* // if loading, display splash screen, else display app */}
-      <SideMenu pageWrapId={"page-wrap"} outerContainerId={"App"} />
+      
       <div id ="page-wrap">
         <NavBar/>
         <div>
--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -1,39 +1,4 @@
-import React from "react";
-import { slide as Menu } from "react-burger-menu";
 
-// Library extension for side menu for now, thinking about using hooks for [open,setOpen] and having a 
-//event listener Onclick 
-// also thinking about using Sass  for a function within the css allowing  transform to change style 
-// like so 
-//  transform : ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
-export default props => {
-  return (
-    // Pass on our props
-    <Menu {...props}>
-      <a className="menu-item" href="/ChapterOne">
-        Chapter 1 Problems
-      </a>
-
-      <a className="menu-item" href="/ChaterTwo">
-        Chapter 2 Problems
-      </a>
-
-      <a className="menu-item" href="/ChapterThree">
-        Chapter 3 Problems
-      </a>
-
-      <a className="menu-item" href="/ChapterFour">
-        Chapter 4 Problems
-      </a>
-      <a className="menu-item" href="/ChapterEight">
-        Chapter 8 Problems
-      </a>
-      <a className="menu-item" href="/ChapterTen">
-        Chapter 10 Problems
-      </a>
-    </Menu>
-  );
-};
 
 
 
